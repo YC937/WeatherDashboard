74ff862d6add2d42424b941696bcd9c6da01dc75
YM C <ymc@cesars-mbp.myfiosgateway.com>
2023-04-27 21:23:45 -0400

    Modified

--- script.js
+++ script.js
@@ -37,7 +37,6 @@ searchButton.click(function () {
             currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
             currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
             var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
-            
             $.ajax({
                 url: urlUV,
                 method: "GET"
@@ -47,7 +46,6 @@ searchButton.click(function () {
                 currentTemp.append(currentUV);
             });
         });
-
         $.ajax({
             url: urlFiveDay,
             method: "GET"
